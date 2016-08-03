a56615c1bb71a4fa6a675d1c9bd6695f21292d98
James Page <james.page@ubuntu.com>
2016-08-03 10:48:50 +0100

    Rework a little

--- bin/run.sh
+++ bin/run.sh
@@ -2,7 +2,9 @@
 
 if [ $(id -u) -eq 0 ]; then
     mkdir -p $SNAP_COMMON/nova.conf.d
-    sed -e "s|__SNAP_DATA__|$SNAP_COMMON|g" $SNAP/templates/nova.conf > $SNAP_COMMON/nova.conf.d/state_path.conf
+    sed -e "s|__SNAP_DATA__|$SNAP_COMMON|g" \
+        -e "s|__SNAP__|$SNAP|g" \
+        $SNAP/templates/nova-snap.conf > $SNAP_COMMON/nova.conf.d/nova-snap.conf
 fi
 
-exec "$SNAP/usr/bin/python2" "$SNAP/usr/bin/$1" --config-file=$SNAP/etc/nova/nova-snap.conf --config-dir=$SNAP_COMMON/nova.conf.d "${@:2}"
+exec "$SNAP/usr/bin/python2" "$SNAP/usr/bin/$1" --config-file=$SNAP/etc/nova/nova.conf --config-dir=$SNAP_COMMON/nova.conf.d "${@:2}"
